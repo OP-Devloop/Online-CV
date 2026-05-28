abf23fa935c207e5d8f527777fa58ff7b644b41e
OP-Devloop <oscar.pettersson@iths.se>
2026-05-28 15:27:28 +0200

    Initial commit

---
+++ src/main/java/se/biteroom/oscp/onlinecv/OnlineCvApplication.java
@@ -0,0 +1,13 @@
+package se.biteroom.oscp.onlinecv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/biteroom/oscp/onlinecv/OnlineCvApplicationTests.java
@@ -0,0 +1,13 @@
+package se.biteroom.oscp.onlinecv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
